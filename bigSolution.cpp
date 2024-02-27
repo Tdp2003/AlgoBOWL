b3cd1e120816558d44bfb604b1f2d4bbf83a595d
tylerpina <tylerpina534@gmail.com>
2024-02-26 21:07:29 -0700

    C implementation 3

--- bigSolution.cpp
+++ bigSolution.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <map>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -20,15 +21,28 @@ void buildGraph(map<int, vector<int> >& g, int& gSize, string file){
 }
 
 void findCycles(map<int, vector<int> >& g, vector<vector<int> >& cycles, vector<int>& path, vector<bool>& visited, int curr){
-    visited.at(curr) = true;
-    path.push_back(curr);
+    visited[curr] = true;
+        path.push_back(curr+1);
+        for (int u : g[curr]) {
+            if (!visited[u-1]) {
+                findCycles(g, cycles, path, visited,u-1);
+            } else {
+                // Found a cycle
+                if (find(path.begin(), path.end(), u) != path.end()) {
+                    vector<int> cycle;
+                    auto it = find(path.begin(), path.end(), u);
+                    for (; it != path.end(); ++it) {
+                        cycle.push_back(*it);
+                        if (*it == u) break;
+                    }
+                    cycles.push_back(cycle);
+                }
+            }
+        }
 
-    for(int i = 0 ; i < g[curr].size(); i++){
-    
-    }
-
-    path.pop_back();
-    visited.at(curr) = false;
+        // Backtrack
+        path.pop_back();
+        visited[curr] = false;
 }
 
 int main(int argc, char* argv[]){
@@ -39,16 +53,15 @@ int main(int argc, char* argv[]){
     vector<bool> visited(gSize, false);
     vector<int> path;
     for(int i = 0; i < gSize; i++){
-        if(!visited.at(i)){
+        if(!visited[i]){
             findCycles(g, cycles, path, visited, i);
         }
     }
 
     for(int i = 0; i < cycles.size(); i++){
         for(int j = 0; j < cycles[i].size(); j++){
-            cout << cycles[i][j];
+            cout << cycles[i][j] << " ";
         }
         cout << endl;
     }
-    
 }
